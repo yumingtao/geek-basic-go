36779df8d3229ea0cbc1d2b2fe1970bffd663fd9
yumingtao <yumingtao@users.noreply.github.com>
2023-11-25 11:15:58 +0800

    Revert "Add example code for work-01 of study (#1)"
    This reverts commit 82542f02cd513f4a68d60100c78f3d748e6ec182.

--- syntax/variables/access/var.go
+++
@@ -1,9 +0,0 @@
-package access
-
-var (
-	Global1  string = "外部可访问"
-	internal string = "外部不可访问"
-)
-var Global2 string = "外部可访问2"
-
-const ConstVariable string = "我是外部constant"
--- syntax/variables/var.go
+++
@@ -1,9 +0,0 @@
-package main
-
-var Global = "我是包变量"
-
-func AccessPackVariables() {
-	println("我在var中：", a)
-	println("我在var中：", b)
-	println("我在var中：", c)
-}
