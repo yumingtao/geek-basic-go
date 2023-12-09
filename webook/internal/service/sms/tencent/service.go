4081f0184a267ca84e1e966b3f279661d312a590
yumingtao <yumingtao2008@gmail.com>
2023-12-09 15:02:16 +0800

    Add send sms.

---
+++ webook/internal/service/sms/tencent/service.go
@@ -0,0 +1,57 @@
+package tencent
+
+import (
+	"context"
+	"fmt"
+	"github.com/ecodeclub/ekit"
+	"github.com/ecodeclub/ekit/slice"
+	sms "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/sms/v20210111"
+)
+
+type Service struct {
+	client   *sms.Client
+	appId    *string
+	signName *string
+}
+
+func NewService(client *sms.Client, appId string, signName string) *Service {
+	return &Service{
+		client:   client,
+		appId:    ekit.ToPtr[string](appId),
+		signName: ekit.ToPtr[string](signName),
+	}
+}
+
+func (s Service) Send(ctx context.Context, tplId string, args []string, numbers ...string) error {
+	request := sms.NewSendSmsRequest()
+	request.SetContext(ctx)
+	request.SmsSdkAppId = s.appId
+	request.SignName = s.signName
+	request.TemplateId = ekit.ToPtr[string](tplId)
+	request.TemplateParamSet = s.toPtrSlice(args)
+	request.PhoneNumberSet = s.toPtrSlice(numbers)
+	response, err := s.client.SendSms(request)
+	if err != nil {
+		fmt.Printf("An API error has returned: %s", err)
+		return err
+	}
+
+	for _, statusPtr := range response.Response.SendStatusSet {
+		if statusPtr == nil {
+			// 不可能进来这里
+			continue
+		}
+		status := *statusPtr
+		if status.Code == nil || *(status.Code) != "Ok" {
+			// 发送失败
+			return fmt.Errorf("短信发送失败！code:%s, msg:%s", *status.Code, *status.Message)
+		}
+	}
+	return nil
+}
+
+func (s Service) toPtrSlice(data []string) []*string {
+	return slice.Map[string, *string](data, func(idx int, src string) *string {
+		return &src
+	})
+}
